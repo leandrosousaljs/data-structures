71e49aa2240142c653727ebbee61226390d48250
Leandro Sousa <156814218+leandrosousaljs@users.noreply.github.com>
2026-09-09 15:26:17 -0300

    refactor to improve findAllPalindromes

--- Strings/findAllPalindromes.js
+++ Strings/findAllPalindromes.js
@@ -1,38 +1,20 @@
-export const findAllPalindromes = (usernames) => {
-  const result = [];
-
-  for (const username of usernames) {
-    if (isPalindrome(username)) {
-      result.push(username);
-    }
-  }
-
-  return result;
-};
+export const findAllPalindromes = (usernames) => usernames.filter(isPalindrome);
 
 const isPalindrome = (username) => {
-  const reversed = reverse(username);
-  const result = username.toLowerCase() === reversed.toLowerCase();
+  const string = username.toLowerCase();
 
-  return result;
-};
-
-const reverse = (string) => {
-  const chars = string.split('');
   let left = 0;
-  let right = chars.length - 1;
+  let right = string.length - 1;
 
   while (left < right) {
-    const temp = chars[left];
-    chars[left] = chars[right];
-    chars[right] = temp;
+    if (string[left] !== string[right]) return false;
+
     left++;
     right--;
   }
 
-  const reversed = chars.join('');
-  return reversed;
+  return true;
 };
 
 // Time complexity: O(n * k)
-// Space complexity: O(n * k)
+// Space complexity: O(n + k)
