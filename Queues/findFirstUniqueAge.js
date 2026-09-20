f371828672585593ee28d8c5f8073cfe1197e2ca
Leandro Sousa <156814218+leandrosousaljs@users.noreply.github.com>
2026-09-20 19:26:56 -0300

    queue lesson p3

--- Queues/findFirstUniqueAge.js
+++ Queues/findFirstUniqueAge.js
@@ -1,10 +1,27 @@
-import { Queue } from './queue';   
+import { Queue } from './queue';
 
 export const findFirstUniqueAge = (ages) => {
-  let queue = new Queue();
+  const queue = new Queue();
+  const ageFrequency = new Map();
 
   for (const age of ages) {
+    queue.enqueue(age);
+    if (!ageFrequency.has(age)) {
+      ageFrequency.set(age, 1);
+    } else {
+      ageFrequency.set(age, ageFrequency.get(age) + 1);
+    }
+  }
+
+  while (!queue.isEmpty()) {
+    const age = queue.dequeue();
+    if (ageFrequency.get(age) === 1) {
+      return age;
+    }
   }
 
   return 'No unique age was found.';
 };
+
+// Time complexity: O(n)
+// Space compelxity: O(n)
